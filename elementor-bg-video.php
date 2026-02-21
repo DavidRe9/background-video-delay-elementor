c5ec2a12393c94ea1903229f10a324a7592eb146
David Kioshi Leite Kinjo <signage.br@gmail.com>
2026-02-21 06:41:56 -0400

    Versão 1.0.2: Corrige carregamento de vídeo após Elementor, overlay e fallback funcionando

--- elementor-bg-video.php
+++ elementor-bg-video.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Background Video Delay for Elementor Improve LCP
  * Description: Add delayed YouTube background videos to Elementor sections for better loading performance, defer video loading for increased page speed.
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: David Kioshi Leite Kinjo
  * Author URI: https://renovesp.wordpress.com/
  * Plugin URI: https://wordpress.org/plugins/background-video-delay-elementor
